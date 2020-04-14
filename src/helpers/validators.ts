b6fdf5666e3a8168b617772ed85ef752c30945d1
commando <ahmedkhalilbejaoui@gmail.com>
2020-04-15 00:27:41 +0200

    add validotr helper

---
+++ src/helpers/validators.ts
@@ -0,0 +1,51 @@
+const validate = (value: string, error: string, rules: any) => {
+  let isValid = true;
+
+  for (const rule in rules) {
+      switch (rule) {
+          case 'minLength': isValid = isValid && minLengthValidator(value, rules[rule]); break;
+          case 'isRequired': isValid = isValid && requiredValidator(value); break;
+          case 'isEmail': isValid = isValid && emailValidator(value); break;
+          default: isValid = true;
+      }
+
+  }
+
+  return {isValid, error};
+}
+
+
+/**
+* minLength Val
+* @param  value
+* @param  minLength
+* @return
+*/
+const minLengthValidator = (value: string, minLength: number) => {
+  return value.length >= minLength;
+}
+
+/**
+* Check to confirm that feild is required
+*
+* @param  value
+* @return
+*/
+const requiredValidator = (value: string) => {
+  return value.trim() !== '';
+}
+
+/**
+* Email validation
+*
+* @param value
+* @return
+*/
+const emailValidator = (value: string) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(value).toLowerCase());
+}
+
+
+
+export default validate;
