ed4aeb7ab5cde24d1ae59f5825f691edda089379
commando <ahmedkhalilbejaoui@gmail.com>
2020-04-15 18:26:04 +0200

    check for form input validity, add styling to example page

--- example/src/App.js
+++ example/src/App.js
@@ -6,22 +6,56 @@ import 'react-formbuilder/dist/index.css'
 const App = () => {
   const formRules = [
     {
-        type: 'text',
-        name: 'userName',
-        placeholder: 'Your name',
-        errorMessage: 'This field is required. Min is 4 characteres!',
-        validationRules: {
-            minLength: 4,
-            isRequired: true
-        },
-        options: {
-          salam: 'ahmed',
-          debounce: 500
-        }
+      type: 'text',
+      name: 'userName',
+      placeholder: 'Your Name',
+      className: 'form-control',
+      errorMessage: 'This field is required. Min is 4 characteres!',
+      validationRules: {
+        minLength: 4,
+        isRequired: true
+      },
+      options: {
+        debounce: 500
+      }
+    },
+    {
+      type: 'email',
+      name: 'email',
+      placeholder: 'Your Email',
+      className: 'form-control',
+      errorMessage: 'This field is required. Email syntax is wrong!',
+      validationRules: {
+        isRequired: true,
+        isEmail: true
+      },
+      options: {
+        debounce: 500
+      }
+    },
+    {
+      type: 'textArea',
+      name: 'description',
+      placeholder: 'Your Message',
+      className: 'form-control',
+      errorMessage: 'This field is required. Min length is 10 charachters!',
+      validationRules: {
+        minLength: 10,
+        isRequired: true
+      },
+      options: {
+        debounce: 500
+      }
     }
   ];
 
-  return <FormBuilder formRules={formRules} />
+  return (
+    <div className="container">
+      <form className="form-group">
+        <FormBuilder formRules={formRules} />
+      </form>
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import { FormRulesInterface } from './FormRulesInterface'
 import { TextField } from './inputs'
 
+
 export const FormBuilder = (props: { [formRules: string]: FormRulesInterface[] }) => {
   const { formRules } = props
   const [values, setValues] = useState({})
@@ -11,9 +12,31 @@ export const FormBuilder = (props: { [formRules: string]: FormRulesInterface[] }
     console.error(values)
   }, [values]);
 
+
+  const isFormRulesValid = () => {
+    const duplicates = formRules.filter((rule, index, self) =>
+      index !== self.findIndex((obj) => (
+        obj.name === rule.name
+      ))
+    )
+
+    if (duplicates && duplicates.length) {
+      return {
+        isValid: false,
+        error: 'Duplicate input field name',
+        duplicates
+      }
+    }
+
+    return {
+      isValid: true
+    }
+  }
+
   const getInputType = ({ type, name, placeholder, errorMessage, options, validationRules, className }: FormRulesInterface, index: number) => {
     switch (type) {
       case 'text':
+      case 'email':
       case 'textArea':
         return (
           <TextField key={index}
@@ -31,9 +54,18 @@ export const FormBuilder = (props: { [formRules: string]: FormRulesInterface[] }
     }
   }
 
+  const { isValid, error, duplicates } = isFormRulesValid()
+
   return (
     <div>
-      {formRules.map((rule, index) => getInputType(rule, index))}
+      {
+        isValid ? formRules.map((rule, index) => getInputType(rule, index)) : (
+          <div>
+            <h3>Form is not valid</h3>
+            <h6>Error: {error}</h6>
+            <h6>Duplicate Name: {duplicates?.map(dup => dup.name).join(', ')}</h6>
+          </div>
+        )}
     </div>
   )
 }
